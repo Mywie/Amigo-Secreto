990d8b0ea3f47ba8b007cc4b86c0cad68770a294
Muriel F <muwuriel@gmail.com>
2025-02-08 00:36:21 -0300

    Finalizacao do projeto

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,26 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+var amigos = [];
+
+function adicionarAmigo(){
+    let nome = document.querySelector('#nome').value;
+    if(nome == ''){
+        alert('Por favor insira um nome.');
+        return;
+    }
+    amigos.push(nome);
+    console.log(amigos);
+    document.querySelector('#nome').value = '';
+}
+
+function sortearAmigo(){
+    let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    console.log(amigoSorteado);
+}
+function mostrarResultado(){
+    let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    document.getElementById('resultado').innerHTML = amigoSorteado;
+}
+
+
+
+
+
